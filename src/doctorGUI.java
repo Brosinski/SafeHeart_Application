bcb6afd434ab2ec13082a114a1e8d24d568c59e4
branchiang <84379781+branchiang@users.noreply.github.com>
2021-05-19 13:11:39 +0200

    added DoctorGUI

---
+++ src/doctorGUI.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class doctorGUI extends JFrame {
+    private JPanel doctorMainPanel;
+    private JButton findPatientButton;
+    private JButton editPatientButton;
+    private JTextField patientCodeText;
+    private JLabel jLabel1;
+    private JLabel riskScoreLabel;
+    private JLabel jLabel2;
+    private JButton findPatientBtn;
+    private JScrollPane patientViewPanel;
+    private JButton calculateBtn;
+
+    public doctorGUI(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(doctorMainPanel);
+        this.pack();
+        calculateBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        findPatientBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new doctorGUI("Doctor");
+        frame.setVisible(true);
+    }
+}
