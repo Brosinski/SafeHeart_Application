e2b233d7ed407ede2bd2ae78d60a2f23c5fd545a
branchiang <84379781+branchiang@users.noreply.github.com>
2021-05-19 19:02:38 +0200

    GUI edit

---
+++ src/recommendationsGUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class recommendationsGUI extends JPanel {
+
+    private JPanel recomMainPanel;
+    private JTextArea exerciseTextArea;
+    private JTextArea dietTextArea;
+    private JButton saveDietBtn;
+    private JButton saveExerciseBtn;
+
+    public recommendationsGUI(){
+        this.setLayout(new GridLayout(1,1));
+        this.add(recomMainPanel);
+
+        saveExerciseBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        saveDietBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+}
